25aa21a70f51e1220ef9204c95f49d3779590e67
raushan211 <raazcrzy90@gmail.com>
2022-07-22 14:51:55 +0530

    vaccination

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -35,6 +36,9 @@ type Vaccinated_Nurses struct {
 type Vaccinated_Persons struct {
 	Email string `json:"email"`
 }
+type Delete struct {
+	Email string `json:"email"`
+}
 
 func setupRoutes(r *gin.Engine) {
 
@@ -42,7 +46,9 @@ func setupRoutes(r *gin.Engine) {
 	r.GET("/persons", redirectPersons)
 	r.GET("/vaccinated_nurses", redirectVaccinatedNurses)
 	r.GET("/vaccinated_persons", redirectVaccinatedPersons)
-
+	r.DELETE("/delete_nurses/", deleteNurses)
+	r.DELETE("/delete_persons/", deletePersons)
+	r.POST("/add_nurse/", addNurse)
 }
 func redirectNurses(c *gin.Context) {
 	Data := []Nurses{}
@@ -144,4 +150,95 @@ func redirectVaccinatedPersons(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, res)
+}
+func deleteNurses(c *gin.Context) {
+	Data := Nurses{}
+	c.Bind(&Data)
+	SQL := "DELETE from nurses where email=$1"
+	_, err := DB.Exec(SQL, Data.Email)
+
+	if err != nil {
+		log.Println("Failed to execute query: ", err)
+		return
+	}
+	//defer rows.Close()
+	//nurse := Nurses{}
+
+	res := gin.H{
+		"nurses": Data,
+	}
+	c.JSON(http.StatusOK, res)
+}
+func deletePersons(c *gin.Context) {
+	Data := Persons{}
+	c.Bind(&Data)
+	SQL := "DELETE from persons where email=$1 "
+	_, err := DB.Exec(SQL, Data.Email)
+
+	if err != nil {
+		log.Println("Failed to execute query: ", err)
+		return
+	}
+	//defer rows.Close()
+	//person := Persons{}
+
+	res := gin.H{
+		"persons": Data,
+	}
+
+	c.JSON(http.StatusOK, res)
+
+}
+func addNurse(c *gin.Context) {
+	reqBody := Persons{}
+	err := c.Bind(&reqBody)
+	if err != nil {
+		res := gin.H{
+			"Error": err.Error(),
+		}
+		c.JSON(http.StatusBadRequest, res)
+		return
+	}
+	result, _ := addNurses(reqBody)
+	if result == false {
+		res := gin.H{
+			"Error": "Something is wromg",
+		}
+		c.JSON(http.StatusBadRequest, res)
+		return
+	}
+	res := gin.H{
+		"message": "successfully inserted",
+		"status":  200,
+	}
+	c.JSON(http.StatusCreated, res)
+}
+func addNurses(reqbody Persons) (bool, string) {
+	var result = true
+	var err_responce = ""
+
+	sqlStatement := `
+INSERT INTO persons(first_name, last_name,email, date_of_birth, sex)
+VALUES ($1, $2, $3, $4,$5)`
+	_, err2 := DB.Exec(sqlStatement, reqbody.First_name, reqbody.Last_name, reqbody.Email, reqbody.Date_of_birth, reqbody.Sex)
+	fmt.Println(err2)
+	if err2 != nil {
+
+		err_responce = "Something went wrong"
+		result = false
+		return result, err_responce
+	}
+
+	sqlStatement2 := `
+INSERT INTO nurses(email) VALUES ($1)`
+	_, err3 := DB.Exec(sqlStatement2, reqbody.Email)
+	fmt.Println(err3)
+	//fmt.Println(user)
+	if err3 != nil {
+
+		err_responce = "Something went wrong"
+		result = false
+		return result, err_responce
+	}
+	return result, err_responce
 }
